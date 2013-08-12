bf7dc8d09c9602f5138aef28b4a017009fa8ef80
Sharron Denice <sharron@me.com>
2013-08-12 11:57:12 -0500

    Enhancement. Updated support link Ticket #ED-3 #3

--- tsp-easy-dev.php
+++ tsp-easy-dev.php
@@ -5,9 +5,9 @@ Plugin URI: 	http://www.thesoftwarepeople.com/software/plugins/wordpress/easy-de
 Description: 	Easy Dev is a <strong>WordPress API</strong>. See <a target="_blank" href="http://lab.thesoftwarepeople.com/tracker/wiki/wordpress-ed:MainPage">API Docs</a> for information and instructions. <a target="_blank" href="https://twitter.com/#bringbackOOD">#bringbackOOD</a> 
 Author: 		The Software People
 Author URI: 	http://www.thesoftwarepeople.com/
-Version: 		1.2.2
+Version: 		1.2.3
 Text Domain: 	tsped
-Copyright: 		Copyright Â© 2013 The Software People, LLC (www.thesoftwarepeople.com). All rights reserved
+Copyright: 		Copyright © 2013 The Software People, LLC (www.thesoftwarepeople.com). All rights reserved
 License: 		APACHE v2.0 (http://www.apache.org/licenses/LICENSE-2.0)
 */
 
@@ -71,9 +71,9 @@ $easy_dev 										= new TSP_Easy_Dev( TSP_EASY_DEV_FILE , TSP_EASY_DEV_REQ_VER
 // Display the parent page but not the options page for this plugin
 $easy_dev->set_options_handler( new TSP_Easy_Dev_Options_Easy_Dev( $easy_dev_settings, true, false ) );
 
-$easy_dev->add_link ( 'FAQ', 					'http://wordpress.org/extend/plugins/tsp-easy-dev/faq/' );
-$easy_dev->add_link ( 'Rate Me', 				'http://wordpress.org/support/view/plugin-reviews/tsp-easy-dev' );
-$easy_dev->add_link ( 'Support', 				'http://wordpress.org/support/plugin/tsp-easy-dev' );
+$easy_dev->add_link ( 'FAQ', 			'http://wordpress.org/extend/plugins/tsp-easy-dev/faq/' );
+$easy_dev->add_link ( 'Rate Me', 		'http://wordpress.org/support/view/plugin-reviews/tsp-easy-dev' );
+$easy_dev->add_link ( 'Support', 		'http://lab.thesoftwarepeople.com/tracker/wordpress-ed/issues/new' );
 
 $easy_dev->set_plugin_icon( TSP_EASY_DEV_ASSETS_IMAGES_URL . 'tsp_icon_16.png' );
 
